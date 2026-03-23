483a86f50d2d29eab5ca9b5b0485d56a92b07fbb
Neal Magee <nem2p@virginia.edu>
2026-03-23 11:51:02 -0400

    Cleanup before class

--- papermill.sh
+++ papermill.sh
@@ -2,5 +2,7 @@
 
 papermill earthquakes.ipynb output.ipynb
 
+# papermill earthquakes.ipynb /dev/null
+
 # papermill earthquakes.ipynb output.ipynb -p starttime '2025-03-01' -p endtime '2025-06-31'
 
